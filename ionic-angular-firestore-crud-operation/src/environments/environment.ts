7686e09225692b9cbd7ff165904c4b35ace125ed
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-08-25 09:32:26 +0200

    Task 2: configuring the ionic application to support firebase

--- ionic-angular-firestore-crud-operation/src/environments/environment.ts
+++ ionic-angular-firestore-crud-operation/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD3-ic2392UuHH5cVE5uuwr6sCJlC7WDUI',
+    authDomain: 'ionic-notes-78b32.firebaseapp.com',
+    projectId: 'ionic-notes-78b32',
+    databaseURL: 'https://ionic-notes-78b32.firebaseio.com/',
+    storageBucket: 'ionic-notes-78b32.appspot.com',
+    messagingSenderId: '26781040083',
+    appId: '1:26781040083:web:7d414e04feabed2f348326',
+  },
 };
 
 /*
