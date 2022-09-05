f0f04a1320a4ba360908b5cf0c0feae37a122bd4
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-09-05 10:25:50 +0200

    Task 21: designing application api service rest end point (movies, tv) with themoviedb

--- ionic-angular-movie-streaming/src/app/services/movie.service.ts
+++ ionic-angular-movie-streaming/src/app/services/movie.service.ts
@@ -1,9 +1,57 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+const API_KEY = '';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  public getGenreList(type: string): Observable<any> {
+    return this.httpClient.get(
+      `https://api.themoviedb.org/3/genre/${type}/list?api_key=${API_KEY}&language=en-US`
+    );
+  }
+
+  public getTrendingList(type: string): Observable<any> {
+    return this.httpClient.get(
+      `https://api.themoviedb.org/3/trending/${type}/day?api_key=${API_KEY}&language=en-US`
+    );
+  }
+
+  public getPoplarList(
+    type: string,
+    page: number,
+    genres: string
+  ): Observable<any> {
+    return this.httpClient.get(
+      `https://api.themoviedb.org/3/${type}/popular?api_key=${API_KEY}&language=en-US&page=${page}&with_genres=${genres}`
+    );
+  }
+
+  public getDetailList(type: string, id: string): Observable<any> {
+    return this.httpClient.get(
+      `https://api.themoviedb.org/3/${type}/${id}?api_key=${API_KEY}&language=en-US`
+    );
+  }
+
+  public getCreditList(type: string, id: string): Observable<any> {
+    return this.httpClient.get(
+      `https://api.themoviedb.org/3/${type}/${id}/credits?api_key=${API_KEY}&language=en-US`
+    );
+  }
+
+  public getVideoList(type: string, id: string): Observable<any> {
+    return this.httpClient.get(
+      `https://api.themoviedb.org/3/${type}/${id}/videos?api_key=${API_KEY}&language=en-US`
+    );
+  }
+
+  public getRecommendationList(type: string, id: string): Observable<any> {
+    return this.httpClient.get(
+      `https://api.themoviedb.org/3/${type}/${id}/recommendations?api_key=${API_KEY}&language=en-US`
+    );
+  }
 }
--- ionic-angular-movie-streaming/src/app/tab1/tab1.page.ts
+++ ionic-angular-movie-streaming/src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
+import { MovieService } from './../services/movie.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  public movieType = 'movie';
 
-  constructor() {}
+  constructor(private movieService: MovieService) {
+    this.initializeSliderContainer();
+  }
 
+  public initializeSliderContainer() {
+    this.movieService.getTrendingList(this.movieType).subscribe((response) => {
+      console.log(response);
+    });
+  }
 }
